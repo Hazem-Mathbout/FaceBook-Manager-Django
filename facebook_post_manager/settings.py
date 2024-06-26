74d225671b86d11c7617228ac2a43a6ca077843e
Hazem Mathbout <hazemmathbout93@gmail.com>
2024-06-26 10:51:34 +0300

    dotenv

--- facebook_post_manager/settings.py
+++ facebook_post_manager/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = os.getenv('DEBUG', default=True, cast=bool)
+DEBUG = os.getenv('DEBUG', default=True)
 
 
 # ALLOWED_HOSTS = [
