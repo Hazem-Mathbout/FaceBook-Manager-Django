789d2c41a739f76eca735b4a50a506c01653a151
Hazem Mathbout <hazemmathbout93@gmail.com>
2024-07-12 19:23:59 +0300

    Test Redis Config

--- facebook_post_manager/settings/production.py
+++ facebook_post_manager/settings/production.py
@@ -18,7 +18,10 @@ DATABASES = {
 }
 
 
-CELERY_BROKER_URL = 'redis://khohi123456789@localhost:6379/0'
+# CELERY_BROKER_URL = 'redis://khohi123456789@localhost:6379/0'
+CELERY_BROKER_URL = 'redis://:123456789@localhost:6379/0'
+
+
 # CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 # CELERY_ACCEPT_CONTENT = ['json']
 # CELERY_TASK_SERIALIZER = 'json'
